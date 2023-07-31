e764106a992ef3f9eb4b72bdbdae9391e28612fe
thevalter <valternunes776@gmail.com>
2023-07-31 20:21:17 -0300

    dark mode theme

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { getLyrics, getMusic } from '../../services/axios';
 import { BiSearch } from "react-icons/bi";
+import Spinner from '../../components/spinner/spinner';
 import './styles.css';
 
 const Home = () => {
@@ -14,7 +15,7 @@ const Home = () => {
 
     await getMusic(`/search?q=${value}`).then(response => {
       setPreview(response.data.data[0].preview);
-      setImage(`https://e-cdns-images.dzcdn.net/images/artist/${response.data.data[0].md5_image}/1280x720-000000-80-0-0.jpg`);
+      setImage(`https://e-cdns-images.dzcdn.net/images/artist/${response.data.data[0].md5_image}/1280x1280-000000-80-0-0.jpg`);
     });
 
     await getLyrics(`/search/?q=${value}`).then(async (response) => {
@@ -25,8 +26,6 @@ const Home = () => {
         setInfo(res.data.song);
       });
     });
-
-    console.log(info);
 
   }
 
@@ -40,12 +39,12 @@ const Home = () => {
         <input placeholder='Digite o nome da musica...' type="text" value={value} onKeyPress={e => e.key == 'Enter' ? search() : null} onChange={event => setValue(event.currentTarget.value)} />
         <BiSearch onClick={search} />
       </div>
-      <div className='music-result'>
+      {preview.length > 3 ? <div className='music-result'>
         <h1>{info.title}</h1>
-        {image.length > 3 ? <img src={image} /> : null}
-        {preview ? <audio controls src={preview} /> : null}
-        <div dangerouslySetInnerHTML={{ __html: info.description ? info.description.html : null }}></div>
-      </div>
+        {image.length > 3 ? <img src={image} /> : <Spinner/>}
+        {preview ? <audio controls src={preview} /> : <Spinner/>}
+        {info.description ? <div dangerouslySetInnerHTML={{ __html: info.description.html}}></div> : <Spinner/>}
+      </div> : null}
       <div className='content' dangerouslySetInnerHTML={{ __html: lyrics }}>
       </div>
     </div>
